349dc55370420a3d4fa4d42b9f7a6501d93657ff
Benoît Guérout <bguerout@gmail.com>
2022-05-18 11:13:50 +0200

    Ajout des TODO

--- server/src/http/routes/confirmationRoutes.js
+++ server/src/http/routes/confirmationRoutes.js
@@ -17,6 +17,8 @@ module.exports = ({ sendEmail }) => {
     tryCatch(async (req, res) => {
       const cfa = req.user;
       if (cfa.statut !== "en attente") {
+        //TODO incrémenter un compteur qui permet de savoir combien de fois il y eu ce problème
+        // Dans le modèle en général ce genre d'information va dans _meta
         throw Boom.badRequest(`Une confirmation a déjà été enregistrée pour le cfa ${cfa.siret}`);
       }
 
@@ -36,6 +38,7 @@ module.exports = ({ sendEmail }) => {
       }).validateAsync(req.body, { abortEarly: false });
 
       await confirm(cfa.siret, email);
+      //TODO envoyer un email de validation de confirmation
       await sendActivationEmails(sendEmail, { username: cfa.username });
 
       return res.json({});
