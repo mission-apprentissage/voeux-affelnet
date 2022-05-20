ac963bcbd14ec193673cba3d32941c7a65322abd
Quentin Petel <QuentinPetel@users.noreply.github.com>
2022-05-20 17:03:38 +0200

    Feat/get emails (#48)
    * Add cli getGestionnaireEmails & getFormateurEmails
    
    * Fix PR

--- server/src/common/api/CatalogueApi.js
+++ server/src/common/api/CatalogueApi.js
@@ -34,6 +34,21 @@ class CatalogueApi extends RateLimitedApi {
       return fetchJson(`${CatalogueApi.baseApiUrl}/entity/formations?${params}`);
     });
   }
+
+  async postFormations(query, options) {
+    return this.execute(async () => {
+      logger.debug(`[${this.name}] Fetching formations...`, query);
+      return fetchJson(`${CatalogueApi.baseApiUrl}/entity/formations`, { ...options, method: "POST" });
+    });
+  }
+
+  async getEtablissements(query, options) {
+    return this.execute(async () => {
+      logger.debug(`[${this.name}] Fetching etablissements...`, query);
+      const params = convertQueryIntoParams(query, options);
+      return fetchJson(`${CatalogueApi.baseApiUrl}/entity/etablissements?${params}`);
+    });
+  }
 }
 
 module.exports = CatalogueApi;
