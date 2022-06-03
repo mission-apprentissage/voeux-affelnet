f4bba6196cda2b298e16e24c920593cdc8ccedd9
Benoît Guérout <bguerout@gmail.com>
2022-06-03 14:38:56 +0200

    Correction du voeux_date dans les établissements

--- server/tests/jobs/importCfas-test.js
+++ server/tests/jobs/importCfas-test.js
@@ -4,6 +4,7 @@ const { Cfa } = require("../../src/common/model");
 const { insertVoeu } = require("../utils/fakeData");
 const FakeReferentielApi = require("../utils/FakeReferentielApi");
 const { createStream } = require("../utils/testUtils");
+const { DateTime } = require("luxon");
 
 const fakeReferentielApi = new FakeReferentielApi([
   {
@@ -48,16 +49,28 @@ describe("importCfas", () => {
     assert.deepStrictEqual(stats, { total: 1, created: 1, updated: 0, invalid: 0, failed: 0 });
   });
 
-  it("Vérifie si le cfa a des voeux", async () => {
+  it("Vérifie que le cfa a des voeux", async () => {
     const cfaCsv = createStream(`siret;email\n11111111100006;contact@lycee.fr`);
-    const relations = createStream(`uai;siret_uai_gestionnaire\n0751234J;11111111100006`);
-    const date = new Date();
+    const relations = createStream(`uai;siret_uai_gestionnaire
+0751234J;11111111100006
+0757890U;11111111100006
+`);
+    const today = new Date();
+    const yesterday = DateTime.now().minus({ days: 1 }).toJSDate();
     await insertVoeu({
       etablissement_accueil: {
         uai: "0751234J",
       },
       _meta: {
-        import_dates: [date],
+        import_dates: [today],
+      },
+    });
+    await insertVoeu({
+      etablissement_accueil: {
+        uai: "0757890U",
+      },
+      _meta: {
+        import_dates: [yesterday],
       },
     });
 
@@ -67,7 +80,8 @@ describe("importCfas", () => {
     });
 
     const found = await Cfa.findOne({ siret: "11111111100006" }, { _id: 0 }).lean();
-    assert.deepStrictEqual(found.etablissements[0].voeux_date, date);
+    assert.deepStrictEqual(found.etablissements[0].voeux_date, today);
+    assert.deepStrictEqual(found.etablissements[1].voeux_date, yesterday);
   });
 
   it("Vérifie qu'on peut ignorer les cfas invalides (pas d'emails)", async () => {
@@ -85,9 +99,20 @@ describe("importCfas", () => {
   });
 
   it("Vérifie qu'on peut mettre à jour uniquement certaines des informations", async () => {
+    const today = new Date();
+
     await importCfas(createStream(`siret;email\n11111111100006;contact@lycee.fr`), {
       relations: createStream(`uai;siret_uai_gestionnaire\n0751234J;11111111100006`),
       referentielApi: fakeReferentielApi,
+    });
+
+    await insertVoeu({
+      etablissement_accueil: {
+        uai: "0751234J",
+      },
+      _meta: {
+        import_dates: [today],
+      },
     });
 
     const stats = await importCfas(createStream(`siret;email\n11111111100006;contact@lycee.fr`), {
@@ -112,10 +137,15 @@ describe("importCfas", () => {
     const found = await Cfa.findOne({ siret: "11111111100006" }, { _id: 0 }).lean();
     assert.strictEqual(found.raison_sociale, "Lycée professionnel 2");
     assert.strictEqual(found.academie.nom, "Paris");
-    assert.deepStrictEqual(
-      found.etablissements.map((e) => e.uai),
-      ["0751234J", "0757890U"]
-    );
+    assert.deepStrictEqual(found.etablissements, [
+      {
+        uai: "0751234J",
+        voeux_date: today,
+      },
+      {
+        uai: "0757890U",
+      },
+    ]);
     assert.deepStrictEqual(stats, { total: 1, created: 0, updated: 1, invalid: 0, failed: 0 });
   });
 
