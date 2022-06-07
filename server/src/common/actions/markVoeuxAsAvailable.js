0f77943cf9d6f7181ddae7f1dd4539f1ea74d29a
Benoît Guérout <bguerout@gmail.com>
2022-06-07 11:24:33 +0200

    Extraction d'une action pour rendre les voeux disponibles

---
+++ server/src/common/actions/markVoeuxAsAvailable.js
@@ -0,0 +1,22 @@
+const { Cfa } = require("../model/index.js");
+const logger = require("../logger.js");
+
+async function markVoeuxAsAvailable(uai, voeuxDate) {
+  const { matchedCount } = await Cfa.updateOne(
+    { "etablissements.uai": uai },
+    {
+      $set: {
+        "etablissements.$.voeux_date": voeuxDate,
+      },
+    },
+    { runValidators: true }
+  );
+
+  if (matchedCount === 0) {
+    logger.warn(`L'établissement d'accueil n'est pas connu dans la base des CFA ${uai}`);
+    return false;
+  }
+  return true;
+}
+
+module.exports = { markVoeuxAsAvailable };
