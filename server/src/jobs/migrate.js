9c7e05bc35d91f71f5ac6fbb1c397a39300a5e86
Benoit Guérout <bguerout@gmail.com>
2022-06-28 09:31:37 +0200

    Correction des dates invalides ($date) (#112)

--- server/src/jobs/migrate.js
+++ server/src/jobs/migrate.js
@@ -4,20 +4,75 @@ const logger = require("../common/logger");
 const { promiseAllProps } = require("../common/utils/asyncUtils");
 // eslint-disable-next-line no-unused-vars
 const { raw } = require("../common/utils/mongooseUtils");
+const { Cfa } = require("../common/model/index.js");
 
-const VERSION = 25;
+const VERSION = 26;
 
-async function removeWriteable() {
-  const { result } = await raw(User).updateMany(
-    {},
-    {
-      $set: {
-        writeable: true,
-      },
-    }
-  );
+async function fixDate() {
+  let updated = 0;
+  let array = await raw(Cfa)
+    .find({
+      $or: [
+        { "voeux_telechargements.date": { $type: "object" } },
+        { "etablissements.voeux_date": { $type: "object" } },
+        { "emails.sendDates": { $elemMatch: { $type: "object" } } },
+        { "anciens_emails.modification_date": { $type: "object" } },
+      ],
+    })
+    .stream();
 
-  return result.nModified;
+  for await (const cfa of array) {
+    cfa.voeux_telechargements = cfa.voeux_telechargements.map((t) => {
+      if (!t.date["$date"]) {
+        return t;
+      }
+
+      return {
+        ...t,
+        date: new Date(t.date["$date"]),
+      };
+    });
+
+    cfa.etablissements = cfa.etablissements.map((e) => {
+      if (!e.voeux_date["$date"]) {
+        return e;
+      }
+
+      return {
+        ...e,
+        voeux_date: new Date(e.voeux_date["$date"]),
+      };
+    });
+
+    cfa.anciens_emails = cfa.anciens_emails.map((ancien) => {
+      if (!ancien.modification_date["$date"]) {
+        return ancien;
+      }
+
+      return {
+        ...ancien,
+        modification_date: new Date(ancien.modification_date["$date"]),
+      };
+    });
+
+    cfa.emails = cfa.emails.map((e) => {
+      return {
+        ...e,
+        sendDates: (e.sendDates || []).map((d) => {
+          if (!d["$date"]) {
+            return d;
+          }
+
+          return new Date(d["$date"]);
+        }),
+      };
+    });
+
+    await raw(Cfa).replaceOne({ siret: cfa.siret }, cfa);
+    updated++;
+  }
+
+  return { updated };
 }
 
 async function hasAlreadyBeenExecuted() {
@@ -33,8 +88,7 @@ async function migrate() {
 
   return promiseAllProps({
     version: VERSION,
-    resetEmails: Promise.all([]),
-    removeWriteable: removeWriteable(),
+    fixDate: fixDate(),
   });
 }
 
