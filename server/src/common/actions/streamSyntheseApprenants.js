ac2a73c134eb719f0d4666017dc25870093e52ce
Benoît Guérout <bguerout@gmail.com>
2022-09-09 14:50:58 +0200

    Correction de la colonne statut dans la synthese

--- server/src/common/actions/streamSyntheseApprenants.js
+++ server/src/common/actions/streamSyntheseApprenants.js
@@ -2,8 +2,17 @@ const { Voeu } = require("../model/index.js");
 const { compose, transformData } = require("oleoduc");
 const { findDossiers } = require("./findDossiers.js");
 const { capitalizeFirstLetter } = require("../utils/stringUtils.js");
-const { ouiNon } = require("../utils/csvUtils.js");
-const { isNil } = require("lodash");
+const { uniq } = require("lodash");
+
+function getJeuneStatut(statuts) {
+  const array = uniq(statuts);
+  if (array.includes(true)) {
+    return "Oui";
+  } else if (array.includes(false)) {
+    return "Non";
+  }
+  return "ND";
+}
 
 async function streamSyntheseApprenants(options = {}) {
   const academies = options.academies;
@@ -16,12 +25,18 @@ async function streamSyntheseApprenants(options = {}) {
           _id: "$apprenant.ine",
           apprenant: { $first: "$apprenant" },
           responsable: { $first: "$responsable" },
-          _meta: { $first: "$_meta" },
+          adresse: { $first: "$_meta.adresse" },
+          jeune_statuts: {
+            $addToSet: "$_meta.jeune_uniquement_en_apprentissage",
+          },
         },
+      },
+      {
+        $sort: { "apprenant.nom": 1, "apprenant.prenom": 1 },
       },
     ]).cursor(),
     transformData(
-      async ({ apprenant, responsable, _meta }) => {
+      async ({ apprenant, responsable, adresse, jeune_statuts }) => {
         const dossiers = await findDossiers(apprenant, responsable);
         const statuts = dossiers.map((d) => d.statut);
         const statut =
@@ -33,14 +48,12 @@ async function streamSyntheseApprenants(options = {}) {
           "Apprenant prénom": apprenant.prenom,
           "Apprenant Téléphone Personnel": apprenant.telephone_personnel,
           "Apprenant Téléphone Portable": apprenant.telephone_portable,
-          "Apprenant Adresse": _meta.adresse,
+          "Apprenant Adresse": adresse,
           "Apprenant Adresse Code Postal": apprenant.adresse.code_postal,
           "Apprenant Adresse Ville": apprenant.adresse.ville,
           "Apprenant Adresse Pays": apprenant.adresse.pays,
           "Statut dans le tableau de bord": capitalizeFirstLetter(statut),
-          "Jeunes uniquement en apprentissage": isNil(_meta.jeune_uniquement_en_apprentissage)
-            ? "ND"
-            : ouiNon(_meta.jeune_uniquement_en_apprentissage),
+          "Jeunes uniquement en apprentissage": getJeuneStatut(jeune_statuts),
         };
       },
       { parallel: 10 }
