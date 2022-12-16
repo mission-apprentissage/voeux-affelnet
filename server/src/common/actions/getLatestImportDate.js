a881c6e20f3eea42bc5d1b74e5ac40484d993baf
Quentin Petel <contact@quentinpetel.fr>
2022-12-16 12:20:34 +0100

    fix: Memory limit error when getting latest import date

--- server/src/common/actions/getLatestImportDate.js
+++ server/src/common/actions/getLatestImportDate.js
@@ -1,12 +1,17 @@
 const { Voeu } = require("../model/index.js");
 
 function getLatestImportDate(Collection = Voeu, filters = {}) {
-  return Collection.aggregate([
-    { $match: filters },
-    { $unwind: "$_meta.import_dates" },
-    { $group: { _id: "$_meta.import_dates" } },
-    { $sort: { _id: -1 } },
-  ]).then((agg) => agg[0]?._id);
+  return Collection.aggregate(
+    [
+      { $match: filters },
+      { $unwind: "$_meta.import_dates" },
+      { $group: { _id: "$_meta.import_dates" } },
+      { $sort: { _id: -1 } },
+    ],
+    {
+      allowDiskUse: true,
+    }
+  ).then((agg) => agg[0]?._id);
 }
 
 module.exports = { getLatestImportDate };
