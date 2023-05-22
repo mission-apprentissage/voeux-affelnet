b9231a32abe110ac2debbe02c85bcea8dc59b1d7
Quentin Petel <contact@quentinpetel.fr>
2023-05-23 00:15:53 +0200

    fix: Forçage du format des siret, uai et adresse courriel lors de la création du fichier des relations

--- server/src/jobs/buildRelationCsv.js
+++ server/src/jobs/buildRelationCsv.js
@@ -6,6 +6,7 @@ const {
   flattenArray,
   mergeStreams,
   filterData,
+  transformData,
 } = require("oleoduc");
 const { Readable } = require("stream");
 const { getRelationsFromOffreDeFormation } = require("./utils/getRelationsFromOffreDeFormation.js");
@@ -41,6 +42,22 @@ async function buildRelationCsv({ outputRelations, outputInvalids }, options = {
 
   await oleoduc(
     mergeStreams(...streams),
+    transformData(({ siret_gestionnaire, email_gestionnaire, uai_etablissement }) => {
+      const sanitized_siret_gestionnaire = siret_gestionnaire?.replace(/\s+/g, "")?.toLowerCase();
+      const sanitized_email_gestionnaire = email_gestionnaire?.replace(/\s+/g, "");
+      const sanitized_uai_etablissement = uai_etablissement
+        ?.replace(/\s+/g, "")
+        ?.toUpperCase()
+        .split(",")
+        .filter((uai) => uai?.length)
+        .map((uai) => uai.toUpperCase());
+
+      return {
+        siret_gestionnaire: sanitized_siret_gestionnaire.length ? sanitized_siret_gestionnaire : undefined,
+        email_gestionnaire: sanitized_email_gestionnaire.length ? sanitized_email_gestionnaire : undefined,
+        uai_etablissement: sanitized_uai_etablissement.length ? sanitized_uai_etablissement : [],
+      };
+    }),
     accumulateData(
       async (accumulator, relation) => {
         const index = accumulator.findIndex((item) => item.siret === relation.siret_gestionnaire);
@@ -50,21 +67,11 @@ async function buildRelationCsv({ outputRelations, outputInvalids }, options = {
           accumulator.push({
             siret: relation.siret_gestionnaire,
             email: relation.email_gestionnaire,
-            etablissements: [
-              ...new Set(
-                relation.uai_etablissement
-                  .split(",")
-                  .map((uai) => uai.toUpperCase())
-                  .filter((uai) => uai?.length)
-              ),
-            ],
+            etablissements: [...new Set(relation.uai_etablissement)],
           });
         } else {
           accumulator[index].etablissements = [
-            ...new Set([
-              ...accumulator[index].etablissements,
-              ...relation.uai_etablissement.split(",").map((uai) => uai.toUpperCase()),
-            ]),
+            ...new Set([...accumulator[index].etablissements, ...relation.uai_etablissement]),
           ];
         }
         return accumulator;
