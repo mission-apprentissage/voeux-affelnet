413330fd71f0d79b89913e39be26df2bdf19bff1
Quentin Petel <contact@quentinpetel.fr>
2024-05-23 10:46:53 +0200

    feat: Suprression de l'envoi de mail d'activation resposnable

--- server/src/http/routes/confirmationRoutes.js
+++ server/src/http/routes/confirmationRoutes.js
@@ -4,18 +4,16 @@ const Joi = require("@hapi/joi");
 const { confirm } = require("../../common/actions/confirm");
 const authMiddleware = require("../middlewares/authMiddleware");
 const tryCatch = require("../middlewares/tryCatchMiddleware");
-const sendActivationEmails = require("../../jobs/sendActivationEmails");
-const resendActivationEmails = require("../../jobs/resendActivationEmails");
-const { User, Responsable } = require("../../common/model");
+const { User } = require("../../common/model");
 
-module.exports = ({ sendEmail, resendEmail }) => {
+module.exports = ({ sendEmail }) => {
   const router = express.Router(); // eslint-disable-line new-cap
   const { checkActionToken, ensureIsOneOf } = authMiddleware();
 
   router.get(
     "/api/confirmation/status",
     checkActionToken(),
-    ensureIsOneOf(["Responsable", "Formateur"]),
+    ensureIsOneOf(["Responsable"]),
     tryCatch(async (req, res) => {
       const user = req.user;
 
@@ -30,19 +28,6 @@ module.exports = ({ sendEmail, resendEmail }) => {
         throw Boom.badRequest(`Une confirmation a déjà été enregistrée pour le compte ${user.username}`);
       }
 
-      if (user.type === "Formateur") {
-        const responsable = await Responsable.findOne({ "etablissements_formateur.uai": user.username });
-
-        const etablissement = responsable.etablissements_formateur?.find(
-          (etablissement) => etablissement.uai === user.username
-        );
-
-        if (!etablissement.diffusion_autorisee) {
-          throw Boom.badRequest(`Aucune délégation de droit n'a été activée pour votre compte ${user.username}`);
-        }
-        user.email = etablissement?.email;
-      }
-
       return res.json({ email: user.email, type: user.type });
     })
   );
@@ -50,7 +35,7 @@ module.exports = ({ sendEmail, resendEmail }) => {
   router.post(
     "/api/confirmation/accept",
     checkActionToken(),
-    ensureIsOneOf(["Responsable", "Formateur"]),
+    ensureIsOneOf(["Responsable"]),
     tryCatch(async (req, res) => {
       const user = req.user;
       const { email } = await Joi.object({
@@ -61,12 +46,6 @@ module.exports = ({ sendEmail, resendEmail }) => {
       await confirm(user.username, email);
 
       await sendEmail({ ...user, email }, "confirmed");
-
-      const previousActivationEmail = user.emails?.find((e) => e.templateName.startsWith("activation_"));
-
-      previousActivationEmail
-        ? await resendActivationEmails(resendEmail, { username: user.username, force: true })
-        : await sendActivationEmails(sendEmail, { username: user.username });
 
       return res.json({});
     })
