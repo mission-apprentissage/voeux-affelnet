cef291f6855b00817ec67fc116aa6d8975d2bee7
Quentin Petel <contact@quentinpetel.fr>
2025-04-15 10:14:24 +0200

    feat: Affichage de l'historique sans modale

--- ui/src/common/components/history/HistoryBlock.jsx
+++ ui/src/common/components/history/HistoryBlock.jsx
@@ -1,14 +1,11 @@
-import { Box, Text, useDisclosure, Link } from "@chakra-ui/react";
-import { getDelegueHistory, /* getFormateurHistory,*/ getRelationHistory, getResponsableHistory } from "./HistoryList";
+import { Text, Link, Box } from "@chakra-ui/react";
+import { getDelegueHistory, getRelationHistory, getResponsableHistory } from "./HistoryList";
 import { HistoryItem } from "./HistoryItem";
-import { HistoryModal } from "./HistoryModal";
+import { useState } from "react";
 
 export const HistoryBlock = ({ relation, responsable, delegue }) => {
-  const {
-    isOpen: isOpenRelationHistoryModal,
-    onOpen: onOpenRelationHistoryModal,
-    onClose: onCloseRelationHistoryModal,
-  } = useDisclosure();
+  const [showMore, setShowMore] = useState(false);
+  const limit = 1;
 
   const responsableHistories = responsable?.histories?.map((history) => getResponsableHistory(history));
   const delegueHistories = delegue?.histories?.map((history) => getDelegueHistory(history));
@@ -18,29 +15,23 @@ export const HistoryBlock = ({ relation, responsable, delegue }) => {
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   );
 
-  const lastHistory = histories[0];
-
   return (
     <>
-      {!!histories[0] && (
+      {!!histories.length && (
         <>
           <Text fontWeight={"bold"}>Historique d'activité</Text>
 
-          <Box mt={2}>
-            <HistoryItem history={lastHistory} />
-          </Box>
-          {!!histories[1] && (
+          {(showMore ? histories : histories.slice(0, limit)).map((history, index) => (
+            <Box mt="2">
+              <HistoryItem mt="2" key={index} history={history} />
+            </Box>
+          ))}
+
+          {histories?.length > limit && (
             <Text mt={2}>
-              <Link variant="action" onClick={onOpenRelationHistoryModal}>
-                Voir l'historique complet
+              <Link variant="action" onClick={() => setShowMore(!showMore)}>
+                {showMore ? <>Voir moins</> : <>Voir l'historique complet</>}
               </Link>
-              <HistoryModal
-                relation={relation}
-                responsable={responsable}
-                delegue={delegue}
-                isOpen={isOpenRelationHistoryModal}
-                onClose={onCloseRelationHistoryModal}
-              />
             </Text>
           )}
         </>
