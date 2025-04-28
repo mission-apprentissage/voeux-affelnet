d598553d585ef2e46f348bab84e7eb5a4dd8551e
Quentin Petel <contact@quentinpetel.fr>
2025-04-28 12:21:14 +0200

    feat: Ajout d'un message d'erreur en cas d'échec au téléchargement des voeux

--- ui/src/common/hooks/responsableHooks.js
+++ ui/src/common/hooks/responsableHooks.js
@@ -1,21 +1,37 @@
 import { useCallback } from "react";
+import { useToast } from "@chakra-ui/react";
 import { getHeaders } from "../httpClient";
 import { downloadCSV } from "../utils/downloadUtils";
 
 export const useDownloadVoeux = ({ responsable: initialResponsable, formateur: initialFormateur, callback }) => {
+  const toast = useToast();
+
   return useCallback(
     async ({ responsable, formateur } = { responsable: initialResponsable, formateur: initialFormateur }) => {
       const filename = `${responsable?.siret}-${formateur?.siret}.csv`;
 
-      const content = await fetch(`/api/responsable/formateurs/${formateur?.siret}/voeux`, {
-        method: "GET",
-        headers: getHeaders(),
-      });
+      let content;
+
+      try {
+        content = await fetch(`/api/responsable/formateurs/${formateur?.siret}/voeux`, {
+          method: "GET",
+          headers: getHeaders(),
+        });
+      } catch (error) {
+        toast({
+          title: "Erreur",
+          description: "Une erreur est survenue lors du téléchargement du fichier.",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+        return;
+      }
 
       downloadCSV(filename, await content.blob());
 
       await callback?.();
     },
-    [initialResponsable, initialFormateur, callback]
+    [initialResponsable, initialFormateur, callback, toast]
   );
 };
